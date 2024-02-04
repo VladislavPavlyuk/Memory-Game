f362b0962577e955f57f3161035cf74eac0415b2
Vladislav Pavlyuk <79658564+VladislavPavlyuk@users.noreply.github.com>
2024-02-04 16:38:15 +0100

    Add files via upload

---
+++ 16_MemoryGame/assets/game.js
@@ -0,0 +1,184 @@
+const selectors = {
+    boardContainer: document.querySelector('.board-container'),
+    board: document.querySelector('.board'),
+    moves: document.querySelector('.moves'),
+    timer: document.querySelector('.timer'),
+    start: document.querySelector('button'),
+    win: document.querySelector('.win')
+}
+
+const state = {
+    gameStarted: false,
+    flippedCards: 0,
+    totalFlips: 0,
+    totalTime: 0,
+    loop: null
+}
+
+const shuffle = array => {
+    const clonedArray = [...array]
+
+    for (let index = clonedArray.length - 1; index > 0; index--) {
+        const randomIndex = Math.floor(Math.random() * (index + 1))
+        const original = clonedArray[index]
+
+        clonedArray[index] = clonedArray[randomIndex]
+        clonedArray[randomIndex] = original
+    }
+
+    return clonedArray
+}
+
+const pickRandom = (array, items) => {
+    const clonedArray = [...array]
+    const randomPicks = []
+
+    for (let index = 0; index < items; index++) {
+        const randomIndex = Math.floor(Math.random() * clonedArray.length)
+        
+        randomPicks.push(clonedArray[randomIndex])
+
+        clonedArray.splice(randomIndex, 1)
+    }
+
+    return randomPicks
+}
+
+
+const generateGame = () => {
+    const dimensions = selectors.board.getAttribute('data-dimension')
+
+    if (dimensions % 2 !== 0) {
+        throw new Error("Количество клеток должно быть четное количество")
+    }
+
+    const images = ['img/1.png',
+                    'img/2.png',
+                    'img/3.png',
+                    'img/4.png',
+                    'img/5.png',
+                    'img/6.png',
+                    'img/7.png',
+                    'img/8.png'];  
+       
+    const picks = pickRandom(images, (dimensions * dimensions) / 2) 
+
+    const items = shuffle([...picks, ...picks])
+
+    const cards = `
+        <div class="board" style="grid-template-columns: repeat(${dimensions}, auto)">
+            ${items.map(item => `
+                <div class="card-main" value="0">
+                    <div class="card-front"></div>
+                    <div class="card-back"><img class="card-main" src="${item}"></div>
+                </div>
+            `).join('')}
+       </div>
+    `
+
+    const showCards = `
+    <div class="board" style="grid-template-columns: repeat(${dimensions}, auto)">
+        ${items.map(item => `
+            <div class="card-main" value="0">
+                <div class="card-front"></div>
+                <div class="card-back"><img class="card-main" src="${item}"></div>
+            </div>
+        `).join('')}
+   </div>
+`
+    
+    const parser = new DOMParser().parseFromString(cards, 'text/html')
+
+    selectors.board.replaceWith(parser.querySelector('.board'))
+}
+
+const stopGame = () => {
+
+}
+const startGame = () => {
+    state.gameStarted = true
+    selectors.start.classList.add('disabled')
+ 
+    state.loop = setInterval(() => {
+        state.totalTime++
+
+        selectors.moves.innerText = `Выполнено ${state.totalFlips} попыток`
+        selectors.timer.innerText = `Затрачено времени: ${state.totalTime} секунд`
+    }, 1000)
+}
+
+const flipBackCards = () => {
+    document.querySelectorAll('.card-main:not(.matched)').forEach(card => {
+        card.classList.remove('flipped')
+    })
+
+    state.flippedCards = 0
+}
+
+const flipCard = card => {
+    state.flippedCards++
+    state.totalFlips++
+
+    if (!state.gameStarted) {
+        startGame()
+    }
+
+    if (state.flippedCards <= 2) {
+        card.classList.add('flipped');
+    }
+
+    if (state.flippedCards === 2) {
+        const flippedCards = document.querySelectorAll('.flipped:not(.matched)')
+
+        if (flippedCards[0].innerHTML === flippedCards[1].innerHTML) {
+            flippedCards[0].classList.add('matched');
+            flippedCards[1].classList.add('matched');
+
+            flippedCards[0].setAttribute('value', '1');
+            flippedCards[1].setAttribute('value', '1');
+        }
+
+        setTimeout(() => {
+            flipBackCards()
+        }, 1000)
+    }
+    else {
+        setTimeout(() => {
+            flipBackCards()
+        }, 2000)
+    }
+
+    const c = document.querySelectorAll('div[value]:not([value="1"])').length
+
+    if (!document.querySelectorAll('div[value]:not([value="1"])').length) {
+        setTimeout(() => {
+            selectors.boardContainer.classList.add('flipped')
+            selectors.win.innerHTML = `
+                <span class="win-text">
+                    Вы победили!<br />
+                    с результатом <span class="highlight">${state.totalFlips}</span> попыток<br />
+                    за <span class="highlight">${state.totalTime}</span> секунд
+                </span>
+            `
+            clearInterval(state.loop)
+        }, 1000)
+    }
+}
+
+const attachEventListeners = () => {
+    document.addEventListener('click', event => {
+        const eventTarget = event.target
+        const eventParent = eventTarget.parentElement
+
+        if (eventTarget.className.includes('card') && !eventParent.className.includes('flipped')) {
+            flipCard(eventParent)
+        } else if (eventTarget.nodeName === 'START' && !eventTarget.className.includes('disabled')) {
+            startGame()
+        } else if (eventTarget.nodeName === 'STOP' && !eventTarget.className.includes('disabled')) {
+            stopGame()
+        })
+}
+
+generateGame()
+
+attachEventListeners()
